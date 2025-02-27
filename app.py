64f08e1656d66cbb4725af7545eb6efc9f9c1af2
lorelay1820 <lorelay1820@hotmail.com>
2025-02-27 07:13:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,14 @@ import streamlit as st
 from PIL import Image
 
 st.title("LOREX")
+
+st.header("En este espacio comienzo a desarollar mis aplicaciones para interfaces multimodales")
+st.write("Facilmente puedo realizar backend y frontend")
+
+image = Image.open (" piolin.png")
+
+st.image(image, caption="piolin")
+
+
+
+      
