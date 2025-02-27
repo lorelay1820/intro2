8332a4f4895849591548708ebd73492ed9aba19b
lorelay1820 <lorelay1820@hotmail.com>
2025-02-27 07:19:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.title("LOREX")
 st.header("En este espacio comienzo a desarollar mis aplicaciones para interfaces multimodales")
 st.write("Facilmente puedo realizar backend y frontend")
 
-image = Image.open (" piolin.png")
+image = Image.open ("silvestren.png")
 
 st.image(image, caption="piolin")
 
