94ba2e7478178525914f2399422a6c6d17cf66d6
lorelay1820 <lorelay1820@hotmail.com>
2025-02-27 07:32:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,20 @@ image = Image.open ("silvestre.png")
 st.image(image, caption="Silvestre y Piolín")
 
 
-texto = st.text_input("Escribe algo", "este es mi text")
+texto = st.text_input("Escribe algo", "Lorexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
 st.write("el texto escrito es", texto)
 
 st.subheader("ahora usemos 2 columnas")
-      
+
+with col1:
+      st.subheader("Esta es la primera columna")
+      st.write("Las interfaces multimodales mejoran la experiencia de usuario")
+      resp=st.checkbox("Estoy de acuerdo")
+      if resp:
+            st.write("Correcto")
+
+with col2
+st.subheader("Esta es la segunda coulumna")
+modo = st.radio("Que Modalidad es la principal en tu interfaz" , ("Visual"," auditiva","Táctil"))
+if modo =="Visual":
+      st.write("La vista es fundamental para tu interfaz")
